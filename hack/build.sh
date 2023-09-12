54db08107526991283e1c917400dbf4e67f5fc23
Jason Cho <jason.cho2@ibm.com>
2023-09-12 12:04:21 -0700

    ppc64le support
    Signed-off-by: Jason Cho <jason.cho2@ibm.com>

---
+++ hack/build.sh
@@ -0,0 +1,97 @@
+#!/usr/bin/env bash
+# builds the operator and its OLM catalog index and pushes it to quay.io.
+#
+# by default, the built catalog index is tagged with
+# `quay.io/projectquay/quay-operator-index:3.6-unstable`. you can override the
+# tag alone by exporting TAG before executing this script.
+#
+# To push to your own registry, override the REGISTRY and NAMESPACE env vars,
+# i.e:
+#   $ REGISTRY=quay.io NAMESPACE=yourusername ./hack/build.sh
+#
+# REQUIREMENTS:
+#  * a valid login session to a container registry.
+#  * `docker`
+#  * `yq`
+#  * `jq`
+#  * `opm`
+#  * `skopeo`
+#
+# NOTE: this script will modify the following files:
+#  - bundle/manifests/quay-operator.clusterserviceversion.yaml
+#  - bundle/metadata/annotations.yaml
+# if `git` is available it will be used to checkout changes to the above files.
+# this means that if you made any changes to them and want them to be persisted,
+# make sure to commit them before running this script.
+set -e
+
+export OPERATOR_NAME='quay-operator'
+export REGISTRY=${REGISTRY:-'quay.io'}
+export NAMESPACE=${NAMESPACE:-'projectquay'}
+export TAG=${TAG:-'v3.6.0'}
+export CSV_PATH=${CSV_PATH:-'bundle/upstream/manifests/quay-bridge-operator.clusterserviceversion.yaml'}
+
+function cleanup {
+	# shellcheck disable=SC2046
+	if [ -x $(command -v git >/dev/null 2>&1) ]; then
+		git checkout "${CSV_PATH}" >/dev/null 2>&1
+		git checkout "${ANNOTATIONS_PATH}" >/dev/null 2>&1
+	fi
+}
+
+trap cleanup EXIT
+
+# prints pre-formatted info output.
+function info {
+	echo "INFO $(date '+%Y-%m-%dT%H:%M:%S') $*"
+}
+
+# prints pre-formatted error output.
+function error {
+	>&2 echo "ERROR $(date '+%Y-%m-%dT%H:%M:%S') $*"
+}
+
+function digest() {
+	declare -n ret=$2
+	IMAGE=$1
+	docker pull "${IMAGE}"
+	# shellcheck disable=SC2034
+	ret=$(docker inspect --format='{{index .RepoDigests 0}}' "${IMAGE}")
+}
+
+docker buildx build --push --platform "linux/amd64,linux/ppc64le"  -t "${REGISTRY}/${NAMESPACE}/quay-bridge-operator:${TAG}" .
+digest "${REGISTRY}/${NAMESPACE}/quay-bridge-operator:${TAG}" OPERATOR_DIGEST
+
+# need exporting so that yq can see them
+export OPERATOR_DIGEST
+
+# prepare operator files, then build and push operator bundle and catalog
+# index images.
+
+yq eval -i '
+	.metadata.name = strenv(OPERATOR_NAME) |
+	.metadata.annotations.quay-version = strenv(TAG) |
+	.metadata.annotations.containerImage = strenv(OPERATOR_DIGEST) |
+	del(.spec.replaces) |
+	.spec.install.spec.deployments[0].name = strenv(OPERATOR_NAME) |
+	.spec.install.spec.deployments[0].spec.template.spec.containers[0].image = strenv(OPERATOR_DIGEST)
+	' "${CSV_PATH}"
+
+make bundle IMG=${OPERATOR_DIGEST}
+docker buildx build --push -f bundle.Dockerfile --platform "linux/amd64,linux/ppc64le"  -t "${REGISTRY}/${NAMESPACE}/quay-bridge-operator-bundle:${TAG}" .
+digest "${REGISTRY}/${NAMESPACE}/quay-bridge-operator-bundle:${TAG}" BUNDLE_DIGEST
+
+AMD64_DIGEST=$(skopeo inspect --raw  docker://${REGISTRY}/${NAMESPACE}/quay-bridge-operator-bundle:${TAG} | \
+               jq -r '.manifests[] | select(.platform.architecture == "amd64" and .platform.os == "linux").digest')
+POWER_DIGEST=$(skopeo inspect --raw  docker://${REGISTRY}/${NAMESPACE}/quay-bridge-operator-bundle:${TAG} | \
+               jq -r '.manifests[] | select(.platform.architecture == "ppc64le" and .platform.os == "linux").digest')
+        
+opm index add --build-tool docker --bundles "${REGISTRY}/${NAMESPACE}/quay-bridge-operator-bundle@${AMD64_DIGEST}" --tag "${REGISTRY}/${NAMESPACE}/quay-bridge-operator-index:${TAG}-amd64" --binary-image "quay.io/operator-framework/opm:v1.28.0-amd64"
+docker push "${REGISTRY}/${NAMESPACE}/quay-bridge-operator-index:${TAG}-amd64"
+opm index add --build-tool docker --bundles "${REGISTRY}/${NAMESPACE}/quay-bridge-operator-bundle@${POWER_DIGEST}" --tag "${REGISTRY}/${NAMESPACE}/quay-bridge-operator-index:${TAG}-ppc64le" --binary-image "quay.io/operator-framework/opm:v1.28.0-ppc64le"
+docker push "${REGISTRY}/${NAMESPACE}/quay-bridge-operator-index:${TAG}-ppc64le"
+
+docker manifest create --amend "${REGISTRY}/${NAMESPACE}/quay-bridge-operator-index:${TAG}" \
+	"${REGISTRY}/${NAMESPACE}/quay-bridge-operator-index:${TAG}-amd64" \
+	"${REGISTRY}/${NAMESPACE}/quay-bridge-operator-index:${TAG}-ppc64le"
+docker manifest push "${REGISTRY}/${NAMESPACE}/quay-bridge-operator-index:${TAG}"
